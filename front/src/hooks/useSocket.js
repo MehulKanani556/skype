dd6061381e24f5f8818bd019f128381e138811dd
AkshayP-kalathiya <padaliya.kalathiyainfotech@gmail.com>
2025-02-26 16:57:15 +0530

    correct

--- front/src/hooks/useSocket.js
+++ front/src/hooks/useSocket.js
@@ -1245,13 +1245,16 @@ export const useSocket = (userId, localVideoRef, remoteVideoRef, allUsers) => {
   // ===========================message reaction=============================
   const addMessageReaction = (message, emoji) => {
     if (!socketRef.current?.connected) return;
-
     socketRef.current.emit("message-reaction", {
       messageId:message._id,
       userId,
       emoji
     });
-    // dispatch(getAllMessages({selectedId:message.receiver}));
+    if(message.receiver === userId){
+      dispatch(getAllMessages({selectedId:message.sender}));
+    }else{
+      dispatch(getAllMessages({selectedId:message.receiver}));
+    }
   };
 
   // ===========================cleanup Connection=============================
